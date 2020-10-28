a4299db7626ac17976abb7d05c44e6fd4a832538
fcu-d0809296 <jiannweyfcutw@mail.fcu.edu.tw>
2020-10-29 03:01:30 +0800

    Done - return data type changed

--- src/main/java/fcu/oop/h1/Main.java
+++ src/main/java/fcu/oop/h1/Main.java
@@ -6,7 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		
 		String weight = scanner.next();
 		String height = scanner.next();
 		scanner.close();
